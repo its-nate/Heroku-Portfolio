df2a4c61a005aab95142018fd87ef5facf7c3e32
Nate Micinski <nmicinski@gmail.com>
2019-03-25 15:50:53 -0700

    refactor server for public files

---
+++ server.js
@@ -0,0 +1,51 @@
+/*
+===============================
+Dependencies
+===============================
+*/
+
+var express = require("express");
+var path = require("path");
+
+// Express App
+var app = express();
+var PORT = process.env.PORT || 4000;
+
+/*
+===============================
+Routes
+===============================
+*/
+
+// Serve static files
+app.use(express.static("public"));
+
+// Root
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+// Index.html
+app.get("/index", function(req, res) {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+// Portfolio.html
+app.get("/portfolio", function (req, res) {
+    res.sendFile(path.join(__dirname, "./portfolio.html"));
+});
+
+// Contact.html
+app.get("/contact", function (req, res) {
+    res.sendFile(path.join(__dirname, "./contact.html"));
+});
+
+/*
+===============================
+Initialize Server Listen
+===============================
+*/
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
