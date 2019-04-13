6924d0e0138e0d10ce20c5142737d0bb68bc69ef
Nate Micinski <nmicinski@gmail.com>
2019-04-13 16:29:31 -0700

    update project card info

--- data.js
+++ data.js
@@ -1,83 +1,107 @@
 var projects = [
         {
-        name: "Yum(Me) | Recipe App",
+        name: "Yum(Me)",
+        desc: "Digital Recipe Book",
+        tech: "Node, Express, MySQL, Sequelize",
         img: "/assets/images/yumme.png",
         alt: "Screen capture of yumme app",
         github: "https://github.com/DearLorditsNate/Yum-me",
         site: "http://www.yumme.recipes"
     },
     {
-        name: "Traveler Check | Travel App",
+        name: "Traveler Check",
+        desc: "Currency Conversion and Translator",
+        tech: "Materialize, OpenWeatherMap API, Open Exchange Rates API, Yandex Translate API",
         img: "/assets/images/traveler-check.png",
         alt: "Screen capture of traveler check app",
         github: "https://github.com/DearLorditsNate/TravelerCheck",
         site: "https://dearlorditsnate.github.io/TravelerCheck/"
     },
     {
-        name: "Eat Da Burger | Node.js, Express.js, MySQL, and Handlebars App",
+        name: "Eat Da Burger",
+        desc: "Create burgers, eat burgers",
+        tech: "Node, Express, MySQL, Handlebars",
         img: "/assets/images/burger.png",
         alt: "Screen capture of Eat Da Burger app",
         github: "https://github.com/DearLorditsNate/burger",
         site: "https://nates-burger.herokuapp.com/"
     },
     {
-        name: "Friend Finder | Node.js and Express.js App",
+        name: "Friend Finder",
+        desc: "Take a quiz, get matched with friends",
+        tech: "Node, Express",
         img: "/assets/images/friend-finder.png",
         alt: "Screen capture of Friend Finder app",
         github: "https://github.com/DearLorditsNate/FriendFinder",
         site: "https://nates-friend-finder.herokuapp.com/"
     },
     {
-        name: "LIRI | Node.js CLI App",
+        name: "LIRI",
+        desc: "Like SIRI but for your CLI",
+        tech: "Node, Inquirer, Spotify API, Bands in Town API, OMDB API",
         img: "/assets/images/liri.png",
         alt: "Screen capture of LIRI CLI app",
         github: "https://github.com/DearLorditsNate/liri-node-app",
         site: "https://youtu.be/Ru-dd_VgaQU"
     },
     {
-        name: "Bamazon | Node.js and MySQL CLI App",
+        name: "Bamazon",
+        desc: "Manage your own digital store through the CLI",
+        tech: "Node, MySQL",
         img: "/assets/images/bamazon.png",
         alt: "Screen capture of Bamazon CLI app",
         github: "https://github.com/DearLorditsNate/bamazon",
         site: "https://youtu.be/kiASpu2PJeo"
     },
     {
-        name: "Star Words | JS Hangman Game",
+        name: "Star Words",
+        desc: "A vanilla Javascript hangman game with a Star Wars theme",
+        tech: "JavaScript, Bootstrap",
         img: "/assets/images/hangman.png",
         alt: "Screen capture of hangman game",
         github: "https://github.com/DearLorditsNate/Word-Guess-Game",
         site: "https://dearlorditsnate.github.io/Word-Guess-Game/"
     },
     {
-        name: "CrystalCollector | JS Number Game",
+        name: "CrystalCollector",
+        desc: "A jQuery-based number matching game.",
+        tech: "jQuery, JavaScript",
         img: "/assets/images/rpg.png",
         alt: "Screen capture of crystal collector game",
         github: "https://github.com/DearLorditsNate/unit-4-game",
         site: "https://dearlorditsnate.github.io/unit-4-game/"
     },
     {
-        name: "Tatooine Trivia | JS Trivia Game",
+        name: "Tatooine Trivia",
+        desc: "Trivia game with a Star Wars theme",
+        tech: "jQuery, JavaScript",
         img: "/assets/images/trivia.png",
         alt: "Screen capture of trivia game",
         github: "https://github.com/DearLorditsNate/TriviaGame",
         site: "https://dearlorditsnate.github.io/TriviaGame/"
     },
     {
-        name: "GifTastic | Giphy API UI",
+        name: "GifTastic",
+        desc: "Find gifs, save gifs",
+        tech: "jQuery, Bootstrap, Giphy API, LocalStorage",
         img: "/assets/images/gif-tastic.png",
         alt: "Screen capture of gif app",
         github: "https://github.com/DearLorditsNate/GifTastic",
         site: "https://dearlorditsnate.github.io/GifTastic/"
     },
     {
-        name: "Train Time | Firebase App",
+        name: "Train Time",
+        desc: "Add and track train schedules",
+        tech: "Firebase, Moment.js, Bootstrap",
         img: "/assets/images/train-time.png",
         alt: "Screen capture of train time app",
         github: "https://github.com/DearLorditsNate/TrainTime",
         site: "https://dearlorditsnate.github.io/TrainTime/"
     },
     {
-        name: "CLI Hangman | Node.js CLI App",
+        name: "CLI Hangman",
+        desc: "Star Wars hangman for the CLI",
+        tech: "Node, Chalk, Inquirer",
         img: "/assets/images/node-hangman.png",
         alt: "Screen capture of Node.js Hangman CLI app",
         github: "https://github.com/DearLorditsNate/constructor-word-guess",
